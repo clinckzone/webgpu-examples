bacef7831146ac4a35540337683b0424836bad97
Divyanshu Tiwari <dtiwari@ar.iitr.ac.in>
2023-11-16 21:32:54 +0530

    Add webpack && Use wgsl file imports into typescript

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 4000;
 
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/shaders', express.static(path.join(__dirname, 'shaders')));
 
 app.get('/', (req, res) => {
 	return res.sendFile(path.join(__dirname, 'index.html'));
